c091c8cfd5882e6cccfe8e9779b92a3d24192e06
wu <1037354808@qq.com>
2018-11-30 15:18:08 +0800

    rout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+// 组件类
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
 }
